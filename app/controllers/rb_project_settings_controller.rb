7fbfd3d6aaf7f8c012f49a6bf05b0c63e41d23b7
xKamuna <Inaki.Minna@gmail.com>
2017-01-10 19:43:51 -0500

    Added project issue status setting to project settings.

--- app/controllers/rb_project_settings_controller.rb
+++ app/controllers/rb_project_settings_controller.rb
@@ -5,6 +5,54 @@ class RbProjectSettingsController < RbApplicationController
   unloadable
 
   def project_settings
+    if Setting.respond_to? :plugin_redmine_project_issue_statuses
+      if(params[:status] != nil && params[:status] != "")
+        issue_status = IssueStatus.new()
+        issue_status.name = params[:status]
+        if Setting.plugin_redmine_project_issue_statuses == nil || Setting.plugin_redmine_project_issue_statuses == ""
+          Setting.plugin_redmine_project_issue_statuses = {'issueStatusToProject' => {}}
+        end
+        if Setting.plugin_redmine_project_issue_statuses['issueStatusToProject'] == nil || Setting.plugin_redmine_project_issue_statuses['issueStatusToProject'] == ""
+          Setting.plugin_redmine_project_issue_statuses['issueStatusToProject'] = {}
+        end
+        if issue_status.save
+          Setting.plugin_redmine_project_issue_statuses['issueStatusToProject'].merge!({issue_status.id => [@project.id]})
+          setting = Setting.find_or_default("plugin_redmine_project_issue_statuses")
+          setting.value = Setting.plugin_redmine_project_issue_statuses
+          setting.save
+          #https://www.redmine.org/issues/9226 by Jan from Planio/Yehuda Katz, updated for the newest version of Redmine by Summer Softleigh.
+          begin
+            WorkflowTransition.transaction do
+              Tracker.all.each do |tracker|
+                Role.all.each do |role|
+                  (IssueStatus.all - [issue_status]).each do |status|
+                    WorkflowTransition.create! :tracker_id => tracker.id, :old_status => issue_status, :new_status => status, :role => role # from
+                    WorkflowTransition.create! :tracker_id => tracker.id, :old_status => status, :new_status => issue_status, :role => role # to
+                  end
+                  WorkflowTransition.create! :tracker_id => tracker.id, :old_status => issue_status, :new_status => issue_status, :role => role # self-to-self
+                end
+              end
+            end
+            flash[:success] = 'Added new issue status.'
+          rescue
+            flash[:warning] = 'Unable to add issue status to all workflows.'
+          end
+        else
+          issue_status = IssueStatus.find_by(name: params[:status])
+          if(params[:status] != "Backlog" && issue_status != nil && Setting.plugin_redmine_project_issue_statuses['issueStatusToProject'].has_key?(issue_status.id))
+            if(!Setting.plugin_redmine_project_issue_statuses['issueStatusToProject'][issue_status.id].respond_to?('include?'))
+              Setting.plugin_redmine_project_issue_statuses['issueStatusToProject'][issue_status.id] = [Setting.plugin_redmine_project_issue_statuses['issueStatusToProject'][issue_status.id]]
+            end
+            if (!Setting.plugin_redmine_project_issue_statuses['issueStatusToProject'][issue_status.id].include?(@project.id))
+              Setting.plugin_redmine_project_issue_statuses['issueStatusToProject'][issue_status.id] << @project.id
+            end
+            flash[:success] = 'Updated project issue status.'
+          else
+            flash[:error] = 'Unable to add issue status.  Maybe the name is invalid or already exists as a global issue status?'
+          end
+        end
+      end
+    end
     enabled = false
     enabled_scrum_stats = false
     if request.post? and params[:settings]
